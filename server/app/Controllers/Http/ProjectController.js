ac66084be7cfebce4e9c3878de9dd46e64e9cb6e
Pedro Carolina <pedrocarolina@polygons-MacBook-Pro-2.local>
2018-12-14 11:42:47 +0000

    Add Delete Route and Exceptions

---
+++ server/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Project = use('App/Models/Project');
+const AuthorizationService = use ('App/Services/AuthorizationService');
+
+class ProjectController {
+  async index({ auth }) {
+    const user = await auth.getUser();
+    console.log(user);
+    return await user.projects().fetch();
+  }
+
+  async create({ auth, request }) {
+    const user = await auth.getUser();
+    const { title } = request.all();
+    const project = new Project();
+    project.title = title;
+    project.fill({
+      title
+    });
+    await user.projects().save(project);
+    return project;
+  }
+
+  async destroy({ auth, request, params})  {
+    const user = await auth.getUser();
+    const id = params.id //same as const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    await project.delete();
+    return project;
+  }
+}
+
+module.exports = ProjectController
--- server/start/routes.js
+++ server/start/routes.js
@@ -18,5 +18,9 @@ const Route = use('Route')
 
 Route.group(() => {
   Route.post('auth/register', 'UserController.register');
-  Route.post('auth/login', 'UserController.login');  
+  Route.post('auth/login', 'UserController.login');
+
+  Route.get('projects', 'ProjectController.index').middleware('auth');
+  Route.post('projects', 'ProjectController.create').middleware('auth');
+  Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
 }).prefix('api');
