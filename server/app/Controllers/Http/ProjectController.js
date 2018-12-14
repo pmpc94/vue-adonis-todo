edfd18b3f51e0dfde7dd941b401c100c377fe44e
Pedro Carolina <pedrocarolina@polygons-MacBook-Pro-2.local>
2018-12-14 12:06:22 +0000

    Add Patch Route

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -30,6 +30,16 @@ class ProjectController {
     await project.delete();
     return project;
   }
+
+  async update({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    project.merge(request.only('title')); //this will only retrieve the 'title' from the body
+    await project.save();
+    return project;
+  }
 }
 
 module.exports = ProjectController
